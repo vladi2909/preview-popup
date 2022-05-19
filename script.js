af289c0125a0942462d2ea42a0b97071fa431af2
Vladimir Kozak <v.kozak@sotbit.ru>
2022-05-19 16:41:27 +0300

    del comments

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
-$(document).ready(function() { // Ждём загрузки страницы
-	$(".landing__link-img").click(function(){	// Событие клика на маленькое изображение
-	  	var img = $(this);	// Получаем изображение, на которое кликнули
-		var src = img.attr('src'); // Достаем из этого изображения путь до картинки
-		$("body").append("<div class='popup'>"+ //Добавляем в тело документа разметку всплывающего окна
+$(document).ready(function() {
+	$(".landing__link-img").click(function(){
+	  	var img = $(this);
+		var src = img.attr('src');
+		$("body").append("<div class='popup'>"+
 						 "<div class='popup-close-wrap'><div class='popup-close'>&times;</div></div>"+
-						 "<div class='popup_bg'></div>"+ // Блок, который будет служить фоном затемненным
-						 "<img src="+src+" class='popup_img' />"+ // Само увеличенное фото
+						 "<div class='popup_bg'></div>"+
+						 "<img src="+src+" class='popup_img' />"+
 						 "</div>"); 
-		$(".popup").fadeIn(200); // Медленно выводим изображение
-		$(".popup_bg").click(function(){	// Событие клика на затемненный фон	   
-			$(".popup").fadeOut(200);	// Медленно убираем всплывающее окно
-			setTimeout(function() {	// Выставляем таймер
-			  $(".popup").remove(); // Удаляем разметку высплывающего окна
+		$(".popup").fadeIn(200);
+		$(".popup_bg").click(function(){   
+			$(".popup").fadeOut(200);
+			setTimeout(function() {
+			  $(".popup").remove();
 			}, 200);
 		});
-		$(".popup-close").click(function(){	// Событие клика на затемненный фон	   
-			$(".popup").fadeOut(200);	// Медленно убираем всплывающее окно
-			setTimeout(function() {	// Выставляем таймер
-			  $(".popup").remove(); // Удаляем разметку высплывающего окна
+		$(".popup-close").click(function(){ 
+			$(".popup").fadeOut(200);
+			setTimeout(function() {
+			  $(".popup").remove();
 			}, 200);
 		});
 	});
